53b06cd86e932987584ea44e23c8a6a1e8b937f9
Eduardo Borges <eduardosorbor@gmail.com>
2021-10-25 12:26:04 -0300

    add: commonInput's component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ButtonFinish from './Buttons/ButtonFinish/ButtonFinish';
-import ButtonMore from './Buttons/ButtonMore/ButtonMore';
-import ButtonNext from './Buttons/ButtonNext/ButtonNext';
-import CheckBox from './CheckBox/CheckBox';
 import Title from './Title/Title';
+import ButtonNext from './Buttons/ButtonNext/ButtonNext';
+import ButtonMore from './Buttons/ButtonMore/ButtonMore';
+import ButtonFinish from './Buttons/ButtonFinish/ButtonFinish';
+import CommonInput from './CommonInput/CommonInput';
 import SelectDay from './SelectInputs/SelectDay/SelectDay';
 import SelectMonth from './SelectInputs/SelectMonth/SelectMonth';
 import SelectYear from './SelectInputs/SelectYear/SelectYear';
+import CheckBox from './CheckBox/CheckBox';
 
 const App = () => {
   return (
@@ -16,9 +17,10 @@ const App = () => {
       {/* <ButtonMore /> */}
       {/* <ButtonFinish /> */}
       {/* <CheckBox label="I accept the terms and privacy" /> */}
-      <SelectDay />
-      <SelectMonth />
-      <SelectYear />
+      {/* <SelectDay /> */}
+      {/* <SelectMonth /> */}
+      {/* <SelectYear /> */}
+      {/* <CommonInput label="Nome" type="text" id="nome" placeholder="Juanito" /> */}
     </>
   );
 };
