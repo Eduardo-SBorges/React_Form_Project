2c0bf8ebddf8d9c0ff1b0b8f38055adff5bb64bb
Eduardo Borges <eduardosorbor@gmail.com>
2021-10-25 11:51:52 -0300

    add: selectInputs' components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import ButtonMore from './Buttons/ButtonMore/ButtonMore';
 import ButtonNext from './Buttons/ButtonNext/ButtonNext';
 import CheckBox from './CheckBox/CheckBox';
 import Title from './Title/Title';
+import SelectDay from './SelectInputs/SelectDay/SelectDay';
+import SelectMonth from './SelectInputs/SelectMonth/SelectMonth';
+import SelectYear from './SelectInputs/SelectYear/SelectYear';
 
 const App = () => {
   return (
@@ -13,6 +16,9 @@ const App = () => {
       {/* <ButtonMore /> */}
       {/* <ButtonFinish /> */}
       {/* <CheckBox label="I accept the terms and privacy" /> */}
+      <SelectDay />
+      <SelectMonth />
+      <SelectYear />
     </>
   );
 };
